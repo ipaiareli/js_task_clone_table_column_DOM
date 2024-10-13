0f8c1f41ecfaf5298c8df2d9dd8e412416ca79c0
Ivan Paiareli <ipayareli@gmail.com>
2024-10-13 14:11:45 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const tableHead = document.querySelector('thead');
+const cloneTableHead = tableHead.cloneNode(true);
+const tableFoot = document.querySelector('tfoot');
+const cloneTableFoot = tableFoot.cloneNode(true);
+const headPosition = cloneTableHead.querySelector('tr :nth-child(2)');
+const footPosition = cloneTableFoot.querySelector('tr :nth-child(2)');
+const headSalary = tableHead.querySelector('tr :nth-child(5)');
+const footSalary = tableFoot.querySelector('tr :nth-child(5)');
+const tableBody = document.querySelectorAll('tbody tr');
+
+headSalary.before(headPosition);
+footSalary.before(footPosition);
+
+tableBody.forEach((tr, index) => {
+  const secondChild = tr.querySelector(':nth-child(2)');
+  const fifthChild = tr.querySelector(':nth-child(5)');
+
+  fifthChild.before(secondChild.cloneNode(true));
+});

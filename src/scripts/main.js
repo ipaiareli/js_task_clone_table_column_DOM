c3d1fafd57e557c5a920189061758ed84015a592
Ivan Paiareli <ipayareli@gmail.com>
2024-10-14 10:46:04 +0300

    add task solution 2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,11 @@
 'use strict';
 
-const tableHead = document.querySelector('thead');
-const cloneTableHead = tableHead.cloneNode(true);
-const tableFoot = document.querySelector('tfoot');
-const cloneTableFoot = tableFoot.cloneNode(true);
-const headPosition = cloneTableHead.querySelector('tr :nth-child(2)');
-const footPosition = cloneTableFoot.querySelector('tr :nth-child(2)');
-const headSalary = tableHead.querySelector('tr :nth-child(5)');
-const footSalary = tableFoot.querySelector('tr :nth-child(5)');
-const tableBody = document.querySelectorAll('tbody tr');
+const table = document.querySelector('table');
+const rows = table.rows;
 
-headSalary.before(headPosition);
-footSalary.before(footPosition);
+for (let i = 0; i < rows.length; i++) {
+  const secondCell = rows[i].cells[1];
+  const clonedCell = secondCell.cloneNode(true);
 
-tableBody.forEach((tr, index) => {
-  const secondChild = tr.querySelector(':nth-child(2)');
-  const fifthChild = tr.querySelector(':nth-child(5)');
-
-  fifthChild.before(secondChild.cloneNode(true));
-});
+  rows[i].insertBefore(clonedCell, rows[i].cells[rows[i].cells.length - 1]);
+}

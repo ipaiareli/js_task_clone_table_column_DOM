c781205d01fa141b581703bd5d2631c01b2a7d3d
Ivan Paiareli <ipayareli@gmail.com>
2024-10-14 10:57:45 +0300

    make build

---
+++ index.9f7e7cea.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table");if(e){var r=Array.from(e.rows),c=document.createDocumentFragment();r.forEach(function(e){var r=e.cells[1];if(r){var c=r.cloneNode(!0);e.insertBefore(c,e.cells[e.cells.length-1])}}),e.appendChild(c)}
+//# sourceMappingURL=index.9f7e7cea.js.map
